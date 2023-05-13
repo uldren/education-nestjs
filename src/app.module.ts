688287c92b8a90b4ff5615659dd936bdf87b1305
Anton Bagrov <dev@ant-bagrov.ru>
2023-05-13 23:59:48 +0300

    подкорректировано подключение к базе данных в зависимости от текущего окружения

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,22 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { FilesModule } from './files/files.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 import { TopPageModule } from './top-page/top-page.module';
-import { FilesModule } from './files/files.module';
+import { SitemapModule } from './sitemap/sitemap.module';
 
 @Module({
 	imports: [
-		ConfigModule.forRoot(),
+		ConfigModule.forRoot({
+			envFilePath: `.env.${process.env.NODE_ENV}`,
+		}),
+
 		MongooseModule.forRoot(
-			`mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`,
+			process.env.NODE_ENV === 'production'
+				? `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`
+				: `mongodb://localhost:27017/test`,
 			{ dbName: process.env.MONGO_DATABASE },
 		),
 		AuthModule,
@@ -21,6 +27,7 @@ import { FilesModule } from './files/files.module';
 		ProductModule,
 		ReviewModule,
 		FilesModule,
+		SitemapModule,
 	],
 	controllers: [AppController],
 	providers: [AppService],
