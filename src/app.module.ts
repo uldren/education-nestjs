0011c80aab80ec566b4d9973cb1fa61322da0748
Anton Bagrov <dev@ant-bagrov.ru>
2023-05-17 05:31:12 +0300

    импортируем telegram модуль

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { getTelegramConfig } from './configs/telegram.config';
 import { FilesModule } from './files/files.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
-import { TopPageModule } from './top-page/top-page.module';
 import { SitemapModule } from './sitemap/sitemap.module';
+import { TelegramModule } from './telegram/telegram.module';
+import { TopPageModule } from './top-page/top-page.module';
 
 @Module({
 	imports: [
@@ -28,6 +30,11 @@ import { SitemapModule } from './sitemap/sitemap.module';
 		ReviewModule,
 		FilesModule,
 		SitemapModule,
+		TelegramModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getTelegramConfig,
+		}),
 	],
 	controllers: [AppController],
 	providers: [AppService],
