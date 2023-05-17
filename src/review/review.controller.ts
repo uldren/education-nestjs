afaa53b43d4cf2cbcb5c8a00d543aafedd9f1408
Anton Bagrov <dev@ant-bagrov.ru>
2023-05-17 05:32:37 +0300

    добавлен метод (роут) рассылки уведомлений в telegram

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -14,19 +14,36 @@ import {
 import { JwtAuthGuards } from '../../src/auth/guards/jwt.guards';
 import { UserEmail } from '../../src/decorators/user-email.decorator';
 import { IdValidationPipe } from '../../src/pipes/id-validation.pipe';
+import { TelegramService } from '../../src/telegram/telegram.service';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { ReviewService } from './review.service';
 
 @Controller('review')
 export class ReviewController {
-	constructor(private readonly reviewService: ReviewService) {}
+	constructor(
+		private readonly reviewService: ReviewService,
+		private readonly telegramService: TelegramService,
+	) {}
 
 	@UseGuards(JwtAuthGuards)
 	@UsePipes(new ValidationPipe({ transform: true }))
 	@Post('create')
 	async create(@Body() dto: CreateReviewDto) {
 		return this.reviewService.create(dto);
+	}
+
+	@UseGuards(JwtAuthGuards)
+	@UsePipes(new ValidationPipe({ transform: true }))
+	@Post('notify')
+	async notify(@Body() dto: CreateReviewDto) {
+		const message = `Имя: ${dto.name}\n
+		Заголовок: ${dto.title}\n
+		Описание: ${dto.description}\n
+		Рейтинг: ${dto.rating}\n
+		ID продукта: ${dto.productId}`;
+
+		return this.telegramService.sendMessage(message);
 	}
 
 	@UseGuards(JwtAuthGuards)
--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ReviewController } from './review.controller';
 import { ReviewSchema } from './review.model';
 import { ReviewService } from './review.service';
+import { TelegramModule } from '../../src/telegram/telegram.module';
 
 @Module({
 	controllers: [ReviewController],
 	providers: [ReviewService],
 	imports: [
 		MongooseModule.forFeature([{ name: 'review', schema: ReviewSchema }]),
+		TelegramModule,
 	],
 })
 export class ReviewModule {}
